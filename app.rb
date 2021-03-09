04f277ca496a8feeea589f80b692d256e444b70d
zfletch <zfletch2@gmail.com>
2021-03-09 09:57:55 -0500

    accept raw xml instead of json

--- app.rb
+++ app.rb
@@ -5,31 +5,23 @@ require 'epi_doc_validator'
 require './lib/config'
 require './lib/errors'
 
+set :strict_paths, false
 set :port, Config::PORT
 set :validator, EpiDocValidator::Validator.new
 
 before { content_type :json }
 
-error JSON::ParserError do
-  json_error('Invalid JSON')
-end
-
-post '/validate/:version?' do
+post '/validate(/:version)?' do
   version = params[:version] || 'latest'
 
   if settings.validator.versions.member?(version)
     request.body.rewind
-    data = JSON.parse(request.body.read)
 
-    if data['xml']
-      settings.validator.errors(data['xml'], version: version).to_json
-    else
-      json_error('Missing "xml" field from JSON')
-    end
+    settings.validator.errors(request.body.read, version: version).to_json
   else
-    json_error(
-      "Version #{version} not supported. " \
-      "Supported versions: #{settings.validator.versions.inspect}",
-    )
+    message = "Version #{version} not supported. " \
+      "Supported versions: #{settings.validator.versions.inspect}"
+
+    halt 422, { error: message }.to_json
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,15 +3,15 @@ RSpec.describe '/validate' do
   let(:invalid_doc) { fixture('invalid_doc.xml') }
   let(:valid9) { fixture('valid_9.xml') }
 
-  it 'returns an empty array when valid' do
-    post '/validate/', { xml: valid_doc }.to_json, as: :json
+  it 'returns an empty array when the EpiDoc XML is valid' do
+    post '/validate/', valid_doc
 
     expect(last_response).to be_ok
     expect(JSON.parse(last_response.body)).to eq([])
   end
 
-  it 'returns an array of errors when there are errors' do
-    post '/validate/', { xml: invalid_doc }.to_json, as: :json
+  it 'returns an array of errors when there are errors in the EpiDoc XML' do
+    post '/validate/', invalid_doc
 
     expect(last_response).to be_ok
     expect(JSON.parse(last_response.body)).to eq([
@@ -20,19 +20,19 @@ RSpec.describe '/validate' do
   end
 
   it 'validates against a different EpiDoc version' do
-    post '/validate/9.1', { xml: valid9 }.to_json, as: :json
+    post '/validate/9.1/', valid9
 
     expect(last_response).to be_ok
     expect(JSON.parse(last_response.body)).to eq([])
 
-    post '/validate/8.23', { xml: valid9 }.to_json, as: :json
+    post '/validate/8.23/', valid9
     expect(JSON.parse(last_response.body)).to eq([
       '-1:-1: ERROR: attribute "toWhom" not allowed at this point; ignored',
     ])
   end
 
-  it 'returns an error when the version is not supported' do
-    post '/validate/nonexistent', { xml: valid_doc }.to_json, as: :json
+  it 'returns an error when the EpiDoc version is not supported' do
+    post '/validate/nonexistent/', valid_doc
 
     expect(last_response).to be_unprocessable
     expect(JSON.parse(last_response.body)).to match(
@@ -42,24 +42,13 @@ RSpec.describe '/validate' do
     )
   end
 
-  it 'returns an error when the JSON is valid but incorrect' do
-    post '/validate/', { not: 'correct' }.to_json, as: :json
+  it 'does not require a trailing slash' do
+    post '/validate', valid_doc
 
-    expect(last_response).to be_unprocessable
-    expect(JSON.parse(last_response.body)).to eq({ 'error' => 'Missing "xml" field from JSON' })
-  end
+    expect(last_response).to be_ok
 
-  it 'returns an error when the JSON is invalid' do
-    post '/validate/', 'string', as: :json
+    post '/validate/9.1', valid9
 
-    expect(last_response).to be_unprocessable
-    expect(JSON.parse(last_response.body)).to eq({ 'error' => 'Invalid JSON' })
-  end
-
-  it 'returns an error when no JSON is given' do
-    post '/validate/'
-
-    expect(last_response).to be_unprocessable
-    expect(JSON.parse(last_response.body)).to eq({ 'error' => 'Invalid JSON' })
+    expect(last_response).to be_ok
   end
 end
